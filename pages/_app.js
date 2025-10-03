d5a5d7ddfca0b5062410aaf502b61152956cfcd4
Robin Painter <lochlanpainter@gmail.com>
2025-10-03 23:16:15 +1000

    more fixes hopefully

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,10 @@
-import '../styles/globals.css'
-import Layout from '../components/Layout'
+import '../styles/globals.css';
+import React from 'react';
 
-export default function MyApp({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+function MyApp({ Component, pageProps }) {
+	return <Component {...pageProps} />;
+}
+
+export default MyApp;
   )
 }
