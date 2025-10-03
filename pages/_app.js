0c14f1052ddbad112dc033f89c8ff48c812e2134
Robin Painter <lochlanpainter@gmail.com>
2025-10-03 23:31:48 +1000

    Yet another attempted fix...

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import '../styles/globals.css';
+import '../css/styles.css';
 import React from 'react';
 
 export default function MyApp({ Component, pageProps }) {
+	// minimal, valid App
 	return <Component {...pageProps} />;
 }
