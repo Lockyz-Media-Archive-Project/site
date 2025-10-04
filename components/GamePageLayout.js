bd1991cb4289f39327e12c9a1bd441ffe647e7d8
Robin Painter <lochlanpainter@gmail.com>
2025-10-04 20:17:00 +1000

    Another quick fix!

--- components/GamePageLayout.js
+++ components/GamePageLayout.js
@@ -6,9 +6,6 @@ import OtherInfo from './OtherInfo';
 import ExternalLinks from './ExternalLinks';
 import Dropdown from './Dropdown';
 import Alert from './Alert';
-import Footer from './Footer';
-import Header from './Header';
-import Navbar from './Navbar';
 
 export default function GamePageLayout({ game, showAnnouncement }){
   return (
