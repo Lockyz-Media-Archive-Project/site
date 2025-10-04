82990473b8c77592f4c50d43f55d3db7b3d18aa0
Robin Painter <lochlanpainter@gmail.com>
2025-10-04 20:38:39 +1000

    Hopefully fixed the dropdown in each game page!

--- components/Dropdown.js
+++ components/Dropdown.js
@@ -4,7 +4,7 @@ export default function Dropdown({ title, children }){
   const [open, setOpen] = useState(false);
   return (
     <div className="dropdown-block">
-      <button className="link-button btn-outline" onClick={()=>setOpen(!open)} aria-expanded={open}>
+      <button className="btn-link" onClick={()=>setOpen(!open)} aria-expanded={open}>
         {title} {open ? '▾' : '▸'}
       </button>
       <div className={`dropdown-content ${open ? 'open' : ''}`} style={{display: open ? 'block' : 'none', marginTop: '8px'}}>
